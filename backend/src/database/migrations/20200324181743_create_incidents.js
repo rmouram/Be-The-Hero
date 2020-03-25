e32b46c06218f912b4a3c1921804ab66cf67ebbe
Romulo Moura <romulomoura@MacBook-Air-de-Jose.local>
2020-03-25 09:32:37 -0300

    metodo post criado

--- backend/src/database/migrations/20200324181743_create_incidents.js
+++ backend/src/database/migrations/20200324181743_create_incidents.js
@@ -14,5 +14,5 @@ exports.up = function(knex) {
 };
 
 exports.down = function(knex) {
-    eturn knex.schema.dropTable('incidents')
+    return knex.schema.dropTable('incidents')
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const crypto = require('crypto')
+const connection = require('./database/connection')
 
 const routes = express.Router()
 
-routes.post("/ongs",(req, res) => {
 
-    const {name, email, whatsapp, ciy, uf} = req.body
+
+routes.post("/ongs",async (req, res) => {
+
+    const {name, email, whatsapp, city, uf} = req.body
 
     const id = crypto.randomBytes(4).toString('HEX')
 
-    return res.json()
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf
+    })
+
+    return res.json({id})
 })
 
 module.exports = routes
