53a03788c8a0ddadb959a57b2ac158464ff82a2e
Romulo Moura <romulomoura@MacBook-Air-de-Jose.local>
2020-03-25 15:29:54 -0300

    login implementado

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,9 +2,12 @@ const express = require("express")
 const Ongcontroller = require('./controller/Ongcontroller')
 const Incidentcontroller = require('./controller/Incidentcontroller')
 const Profilecontroller = require('./controller/Profilecontroller')
+const Sessioncontroller = require('./controller/Sessioncontroller')
 
 
 const routes = express.Router()
+
+routes.post("/session", Sessioncontroller.create)
 
 routes.get("/ongs", Ongcontroller.list)
 routes.post("/ongs", Ongcontroller.create)
