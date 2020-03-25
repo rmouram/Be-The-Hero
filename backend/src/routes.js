4fccd4571838d1bab9566651965f319bfdee28a1
Romulo Moura <romulomoura@MacBook-Air-de-Jose.local>
2020-03-25 11:03:58 -0300

    metodos post e get de incidents criados

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const Ongcontroller = require('./controller/Ongcontroller')
+const Incidentcontroller = require('./controller/Incidentcontroller')
+
 
 const routes = express.Router()
 
 routes.get("/ongs", Ongcontroller.list)
 routes.post("/ongs", Ongcontroller.create)
+
+routes.get("/incidents", Incidentcontroller.list)
+routes.post("/incidents", Incidentcontroller.create)
 
 module.exports = routes
