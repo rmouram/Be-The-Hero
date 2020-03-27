cd5f523bd7407d6ecd8add3f8b6c6a926da37598
Romulo Moura <romulomoura@MacBook-Air-de-Jose.local>
2020-03-27 16:35:11 -0300

    tela de logon e rotoas criadas

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import './styles.css'
+import {FiLogIn} from "react-icons/fi"
+
+import heroesImg from "../../assets/heroes.png"
+import logoImg from "../../assets/logo.svg"
+
+export default function Logon(){
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero"/>
+                <form>
+                    <h1>Faça seu logon</h1>
+                    <input placeholder="Sua ID"/>
+                    <button className='button' type="submit">Entrar</button>
+
+                    <a href="/Register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho cadastro!
+                    </a>
+                </form>
+            </section>
+            <img src={heroesImg} alt="heroes"/>
+        </div>
+    )
+}
